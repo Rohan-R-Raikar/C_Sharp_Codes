7b14e4350656f91389c0cb3f2a859fc2f560abb1
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-02 14:00:19 +0530

    Comments and Printing Output in different type also you can add Cosole.ReadKey(); to avoid the Extra things that pops at end of the Project

---
+++ 02FormatingOutputInC#/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FormatingOutputInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hey!");
+            Console.WriteLine("Hello!");
+
+            // This is a Comment
+
+            /*
+             * this
+             * is
+             * a
+             * multiline
+             * comment
+             */
+
+            Console.WriteLine("\tBro\bCode\n");
+
+            Console.ReadKey();
+        }
+    }
+}
