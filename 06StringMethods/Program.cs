b72b180ccbc00c22bc4c66c4b4cd4f50cb9d0919
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-03 10:51:50 +0530

    06StringMethods Project added Done Coding related to it

---
+++ 06StringMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace StringMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fullName = "Bro Code";
+            Console.WriteLine(fullName);
+            String phoneNumber = "123-456-7890";
+            Console.WriteLine(phoneNumber);
+            String fullNameUp = fullName.ToUpper();
+            Console.WriteLine(fullNameUp);
+            String fullNameLow = fullName.ToLower();
+            Console.WriteLine(fullNameLow);
+            String phoneNumberNormal = phoneNumber.Replace("-","");
+            Console.WriteLine(phoneNumberNormal);
+            String UserName = fullName.Insert(0,"Mr.");
+            Console.WriteLine(UserName);
+            int nameLength = fullName.Length;
+            Console.WriteLine(nameLength);
+
+            String firstName = fullName.Substring(0, 3);
+            String lastName = fullName.Substring(4, 4);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
