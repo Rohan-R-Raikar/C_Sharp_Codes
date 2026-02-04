41c0f8babbabc02008eb604f772f951450d25c39
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-04 15:35:55 +0530

    Created a Project called 10MethodsReturnkeywordMethodOverloadingParamskeywordExceptionhandling from that Methods and Return Keyword is Complete

---
+++ 10MethodsReturnkeywordMethodOverloadingParamskeywordExceptionhandling/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace MethodsReturnkeywordMethodOverloadingParamskeywordExceptionhandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // method = performs a section of code, whenever it's called "invoked"
+            //          benefit = Let's reuse code w/o writing it multiple times
+
+            String name = "Bro";
+            int age = 25;
+            // arguments = what is sent to methods
+
+            singHappyBday(name, age);
+            singHappyBday(name, age);
+            singHappyBday(name, age);
+
+            // return = returns data back to the place where a method is invoked
+            double x;
+            double y;
+            double result;
+
+            Console.WriteLine("Enter in number 1: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter in number 2: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            result = Muliply(x, y);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+        static double Muliply(double x, double y)
+        {
+            double z = x * y;
+            return z;
+        }
+
+        static void singHappyBday(String birthdayBoy, int yearOld)
+        {
+            Console.WriteLine("Happy Birthday to you!");
+            Console.WriteLine("Happy Birthday to you!");
+            Console.WriteLine("Happy Birthday dear "+birthdayBoy+"!");
+            Console.WriteLine("You are going to turn "+ (yearOld + 1));
+            Console.WriteLine("CONGRATULATIONS");
+            Console.WriteLine("Happy Birthday to you!");
+            Console.WriteLine();
+        }
+    }
+}
