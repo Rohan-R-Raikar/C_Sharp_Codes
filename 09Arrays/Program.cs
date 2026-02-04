a6b94150c427c106ec2816038bd550051f0073a4
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-04 14:18:48 +0530

    learned how the Foreach Loop Works

--- 09Arrays/Program.cs
+++ 09Arrays/Program.cs
@@ -7,6 +7,7 @@
             // array = a variable that can store multiple values. fixed size
 
             //String[] cars = {"BMW","Mustang","Corvette"};
+            //cars[0] = "Tesla";
 
             String[] cars = new string[3];
 
@@ -14,10 +15,18 @@
             cars[1] = "Mustang";
             cars[2] = "Corvette";
 
-            for(int i=0; i<cars.Length;i++)
+            for (int i = 0; i < cars.Length; i++)
             {
                 Console.WriteLine(cars[i]);
             }
+
+            // foreach loop = a simpler way to iterate over an array, but it's lesss flexible
+
+            foreach(String car in cars)
+            {
+                Console.WriteLine(car);
+            }
+            Console.ReadKey();
         }
     }
 }
