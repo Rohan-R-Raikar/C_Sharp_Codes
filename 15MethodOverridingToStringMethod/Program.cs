c50bca9ad67a7bc33f9c40d574427b7a4952a1fb
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-05 14:17:46 +0530

    Method Overriding

---
+++ 15MethodOverridingToStringMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Method Overriding = provides a new version of a method inherited from a parent class
+            //                     inherited method must be abstract,virtual,or already overriden
+            //                     Used with ToString(), polymorphism
+
+            // For abstract classes or methods
+
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+
+            // ToString() = converts an object to its string representation so that it is suitable
+            //              for display
+
+            Console.ReadKey();
+        }
+    }
+    class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("The Animal Goes *brrrrrrrrrrrr*");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Bhau Bhau");
+        }
+    }
+    class Cat : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Mau Mau");
+        }
+    }
+}
