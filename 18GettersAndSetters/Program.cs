0e7c75fcbd0a71120252d862980aeb8778f7b621
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-02-05 17:53:12 +0530

    18th Project

---
+++ 18GettersAndSetters/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace GettersAndSetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * getters & setters = add security to fields by encapsulation
+             *                     They're accessors found within properties
+             * properties = combine aspects of both fields and methods (share name with a field)
+             * get accessor = used to return the property value
+             * get accessor = used to assign a new value
+             * value keyword = defines the value being assigned by the set (parameter)
+             */
+
+            Car car = new Car(400);
+
+            car.Speed = 1000;
+
+            Console.WriteLine(car.Speed);
+
+            // Auto-Implemented properties = shortcut when no additional logic is requiered in the property
+            //                               you do not have to define a field for a property,
+            //                               you only have to write get; and/or set; inside the property
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        private int speed;
+
+        public Car(int speed)
+        {
+            Speed = speed;
+        }
+
+        public int Speed
+        {
+            get { return speed; }
+            set 
+            { 
+                if (value > 500) 
+                {
+                    speed = 500;
+                }
+                else
+                {
+                    speed = value;
+                }
+
+            }
+        }
+    }
+}
